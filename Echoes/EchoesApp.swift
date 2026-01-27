714dbacd79adf7522f4cab7c2031a8d825683780
sena <furuta.sena@gmail.com>
2026-01-27 19:54:03 +0900

    Initial Commit

---
+++ Echoes/EchoesApp.swift
@@ -0,0 +1,17 @@
+//
+//  EchoesApp.swift
+//  Echoes
+//
+//  Created by 古田聖直 on 2026/01/27.
+//
+
+import SwiftUI
+
+@main
+struct EchoesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
